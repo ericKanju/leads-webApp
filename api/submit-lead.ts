7f13a8f5eeb5607d41139f2c7f6143621ec78275
kanju <erickanju@gmail.com>
2026-03-19 07:19:37 +0300

    fix: use CommonJS export for Vercel function

--- api/submit-lead.ts
+++ api/submit-lead.ts
@@ -3,7 +3,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 
 import { pool } from './_db';
 
-export default async function handler(req: VercelRequest, res: VercelResponse) {
+module.exports = async function handler(req: VercelRequest, res: VercelResponse) {
 
   // CORS headers
   res.setHeader('Access-Control-Allow-Origin', '*');
