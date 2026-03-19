1dfd0c9d99f9ba568d38565bad3336a9853c91b4
kanju <erickanju@gmail.com>
2026-03-19 10:48:51 +0300

    fix: Enable strict SSL certificate validation for database connections.

--- api/_db.js
+++ api/_db.js
@@ -13,7 +13,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: 'postgres://postgres.iyovhgezlapavfaufxyt:QCgkjbR6Qd5VrSdM@aws-1-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x',
-  ssl: { rejectUnauthorized: false }
+  ssl: { rejectUnauthorized: true }
 });
 
 module.exports = { pool };
