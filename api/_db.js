487f3e7ed1cae7284fe9b7934e3cb0dfc52cd52e
kanju <erickanju@gmail.com>
2026-03-19 10:54:41 +0300

    update db

--- api/_db.js
+++ api/_db.js
@@ -12,7 +12,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgres://postgres.iyovhgezlapavfaufxyt:QCgkjbR6Qd5VrSdM@aws-1-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x',
+  connectionString: process.env.leadsDb_POSTGRES_URL,
   ssl: { rejectUnauthorized: true }
 });
 
