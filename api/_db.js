670751cc7c04c597227dcc9642862b88b81d9bd8
kanju <erickanju@gmail.com>
2026-03-19 10:45:06 +0300

    feat: Hardcode Supabase database connection string.

--- api/_db.js
+++ api/_db.js
@@ -12,7 +12,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+  connectionString: 'postgres://postgres.iyovhgezlapavfaufxyt:QCgkjbR6Qd5VrSdM@aws-1-us-east-1.pooler.supabase.com:6543/postgres?sslmode=require&supa=base-pooler.x',
   ssl: { rejectUnauthorized: false }
 });
 
